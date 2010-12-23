dbf2f11b51e7439ec190eb11d132b66d87958c47
Gabriel Grant <g@briel.ca>
2010-12-23 14:10:15 -0500

    factored out the view's default condition_func; fixed whitespace and other minor bugs

--- inline_edit/views.py
+++ inline_edit/views.py
@@ -1,6 +1,5 @@
-from django.views import View, DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView
 from django.utils.decorators import classonlymethod
-from django.models import Permission
 from django.core.exceptions import ImproperlyConfigured
 
 
@@ -23,10 +22,10 @@ class ConditionalDispatchView(object):
 	class Meta:
 		pass
 	@classonlymethod
-	def as_view(cls, condition_func=None,
+	def as_view(cls, condition_func=None, condition_func_factory=None,
 		true_view_class=None, false_view_class=None, **initkwargs
 	):
-		if true_view_class is None
+		if true_view_class is None:
 			try:
 				true_view_class = cls.Meta.true_view_class
 			except AttributeError:
@@ -35,7 +34,7 @@ class ConditionalDispatchView(object):
 					" True. Either provide a true_view_class, or define a"
 					" true_view_class property on this view's Meta class."
 				)
-		if false_view_class is None
+		if false_view_class is None:
 			try:
 				false_view_class = cls.Meta.false_view_class
 			except AttributeError:
@@ -44,34 +43,52 @@ class ConditionalDispatchView(object):
 					" False. Either provide a false_view_class, or define a"
 					" false_view_class property on this view's Meta class."
 				)
-        
-        # get properties defined on this subclass as base initkwargs
-        base_initkwargs = dict_diff(cls, ConditionalDispatchView)
-        base_initkwargs.update(initkwargs)
-        
-        # remove the Meta class; that's for us, not the view class
-         base_initkwargs.pop('Meta', None)
-        
-        # create class-specific keyword arguments
+		
+		# get properties defined on this subclass as base initkwargs
+		base_initkwargs = dict_diff(cls.__dict__, ConditionalDispatchView.__dict__)
+		base_initkwargs.update(initkwargs)
+		
+		# remove the Meta class; that's for us, not the view class
+		base_initkwargs.pop('Meta', None)
+		
+		# create class-specific keyword arguments
 		true_initkwargs = {}
 		false_initkwargs = {}
-        
-        for k, v in base_initkwargs.iter_items():
-            if hasattr(true_view_class, k):
-                true_initkwargs[key] = v
-            if hasattr(false_view_class, k):
-                false_initkwargs[key] = v
 		
-		true_view = true_view_class().as_view(**true_initkwargs)
-		false_view = false_view_class().as_view(**false_initkwargs)
+		for k, v in base_initkwargs.iteritems():
+			if hasattr(true_view_class, k):
+				true_initkwargs[k] = v
+			if hasattr(false_view_class, k):
+				false_initkwargs[k] = v
+		
+		true_view = true_view_class.as_view(**true_initkwargs)
+		false_view = false_view_class.as_view(**false_initkwargs)
 		
 		if condition_func is None:
-			# default condition_func checks if user 'can_change' the object
-			model = true_view_class(**true_initkwargs).get_queryset().model
-			app_label = model._meta.app_label
-			change_perm = model._meta.get_change_permission()
-			def condition_func(request, *args, **kwargs)
-				return request.user.has_perm(app_label + '.' + change_perm)
+			try:
+				condition_func = cls.Meta.condition_func
+			except AttributeError:
+				if condition_func_factory is None:
+					try:
+						condition_func_factory = cls.Meta.condition_func_factory
+					except AttributeError:
+						raise ImproperlyConfigured(
+							"No condition_func or condition_func_factory has been"
+							" defined. Provide one of them, either as a kwarg"
+							" to as_view or as a property on this view's Meta class."
+						)
+				# construct a condition func for this view's model
+				try:
+					true_model = true_view_class(**true_initkwargs).get_queryset().model
+				except AttributeError:
+					true_model = None
+				try:
+					false_model = false_view_class(**false_initkwargs).get_queryset().model
+				except AttributeError:
+					false_model = None
+				condition_func = condition_func_factory(true_model, false_model)
+				if not callable(condition_func):
+					raise TypeError('condition_func must be callable')
 		
 		return conditional_dispatch(condition_func, true_view, false_view)
 
@@ -80,5 +97,12 @@ class InlineUpdateView(ConditionalDispatchView):
 	class Meta:
 		true_view_class = UpdateView
 		false_view_class = DetailView
-
+		@staticmethod
+		def condition_func_factory(true_model, false_model):
+			# default condition_func checks if user 'can_change' the object
+			app_label = true_model._meta.app_label
+			change_perm = true_model._meta.get_change_permission()
+			def condition_func(request, *args, **kwargs):
+				return request.user.has_perm(app_label + '.' + change_perm)
+		
 

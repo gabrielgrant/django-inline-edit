ffe851541098b35d44e5f83b5f1faf89e0a9ec25
Gabriel Grant <g@briel.ca>
2011-02-04 02:06:15 -0500

    update inline_edit.js:
    - use iedit as prefix, rather than inline_edit
    - support data-edit-selector as well as href
    - support slide reveal/hide animation
    - support the selection container being the form or content
      (rather than requiring a seperate containing element)
    - do setup for ajax form submission (though it is not yet fully supported)

--- inline_edit/static/inline_edit/inline_edit.js
+++ inline_edit/static/inline_edit/inline_edit.js
@@ -1,10 +1,43 @@
+/*Load jQuery if not already loaded*/
+if(typeof jQuery == 'undefined'){
+	document.write("<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\"></script>");
+	var __noconflict = true;
+}
+//'<div class="context-loader">Saving&hellip;</div>'
 
-jquery_wrapper($){
+
+(function ($){
 	$(function(){
-		$("inline_edit_button").click(function(){
-			editable = $(this).attr("href");
-			$(editable).find("inline_editable_content").hide();
-			$(editable).find("inline_editable_form").show();
+		// do visibility setup
+		$(".iedit_form").hide();
+		$(".iedit_button").click(function(){
+			var editable_sel = $(this).attr("data-edit-selector");
+			if (editable_sel == undefined){
+				editable_sel = $(this).attr("href");
+			}
+			var editable = $(editable_sel).find("*").andSelf();
+			duration = $(this).attr("data-edit-slide");
+			if (duration == undefined){
+				editable.filter(".iedit_content").toggle();
+				editable.filter(".iedit_form").toggle();
+			}
+			else {
+				editable.filter(".iedit_content").slideToggle(duration);
+				editable.filter(".iedit_form").slideToggle(duration);
+			}
+			return false;
+		});
+
+		// do form submission setup
+		if ( !!$['ajaxForm'] ){
+			var forms = $("form.iedit_form").add(
+				$(".iedit_form").parents("form")  // parent forms
+			).add(
+				$(".iedit_form form")  // child forms
+			);
+			forms.ajaxForm(function() {
+				alert("Successfully saved.");
+			});
 		}
 	});
-)(jquery);
+})(jQuery);
